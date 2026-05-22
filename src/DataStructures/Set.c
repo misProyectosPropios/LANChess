54c5a288ad7707c01ba4443121546aa92df32f34
misProyectosPropios <inakifrutos@outlook.com>
2026-05-22 02:26:04 +0000

    Use _Int for each set int

--- src/DataStructures/Set.c
+++ src/DataStructures/Set.c
@@ -1,35 +1,35 @@
 #include "ArrayList.h"
 #include "Set.h"
 Set* createSet(int capacity) {
-    ArrayList* data = createArray();
+    ArrayList_Int* data = createArray_Int();
     Set* set = malloc(sizeof(Set));
     set->elements = data;
     return set;
 }
 
 void addElement(Set* set, int element) {
-    if (contains(set->elements, element)) {
+    if (contains_Int(set->elements, element)) {
       return;
     }
-    add(set->elements, element);
+    add_Int(set->elements, element);
 }
 
 void removeElement(Set* set, int element) {
-  int index = indexOf(set->elements, element);
+  int index = indexOf_Int(set->elements, element);
   if (index != -1) {
-    removeItem(set->elements, index);
+    removeItem_Int(set->elements, index);
   }
 }
 
 int containsElement(Set* set, int element) {  
-    return contains(set->elements, element);
+    return contains_Int(set->elements, element);
 }
 
 void freeSet(Set* set) {
-    freeList(set->elements);
+    freeList_Int(set->elements);
 }
 
 int getSize(Set* set) {
-  return size(set->elements);
+  return size_Int(set->elements);
 }
 
