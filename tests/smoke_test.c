6ab1dd7a413179947288730c4bb92f2c908b4275
misProyectosPropios <inakifrutos@outlook.com>
2026-05-22 02:30:30 +0000

    Add tests to chess

--- tests/smoke_test.c
+++ tests/smoke_test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "app_info.h"
 #include "board.h"
@@ -33,6 +34,65 @@ static void perform_move(int prevrow, int prevcol, int nextrow, int nextcol, Boa
         .to = { .row = nextrow, .col = nextcol }
     };
     move(board, movement);
+}
+
+static void clear_board(Board *board)
+{
+    for (int row = 0; row < LANCHESS_BOARD_SIZE; row++) {
+        for (int col = 0; col < LANCHESS_BOARD_SIZE; col++) {
+            board->squares[row][col] = (Piece){ PIECE_NONE, COLOR_NONE };
+        }
+    }
+}
+
+static void set_piece(Board *board, int row, int col, PieceType type, PieceColor color)
+{
+    board->squares[row][col] = (Piece){ type, color };
+}
+
+static bool positions_equal(BoardPosition a, BoardPosition b)
+{
+    return a.row == b.row && a.col == b.col;
+}
+
+static bool king_is_in_check(Board *board, PieceColor kingColor)
+{
+    BoardPosition kingPos = { .row = -1, .col = -1 };
+    for (int row = 0; row < LANCHESS_BOARD_SIZE; row++) {
+        for (int col = 0; col < LANCHESS_BOARD_SIZE; col++) {
+            Piece piece = board->squares[row][col];
+            if (piece.type == PIECE_KING && piece.color == kingColor) {
+                kingPos.row = row;
+                kingPos.col = col;
+            }
+        }
+    }
+
+    if (kingPos.row < 0) {
+        return false;
+    }
+
+    PieceColor opponentColor = kingColor == COLOR_WHITE ? COLOR_BLACK : COLOR_WHITE;
+    for (int row = 0; row < LANCHESS_BOARD_SIZE; row++) {
+        for (int col = 0; col < LANCHESS_BOARD_SIZE; col++) {
+            Piece piece = board->squares[row][col];
+            if (piece.color != opponentColor) {
+                continue;
+            }
+
+            PossiblePositions targets = movements(board, (BoardPosition){ .row = row, .col = col });
+            for (int i = 0; i < (int)size_BoardPosition(targets.possiblePositions); i++) {
+                BoardPosition target = get_BoardPosition(targets.possiblePositions, i);
+                if (positions_equal(target, kingPos)) {
+                    destroy_BoardPosition(targets.possiblePositions);
+                    return true;
+                }
+            }
+            destroy_BoardPosition(targets.possiblePositions);
+        }
+    }
+
+    return false;
 }
 
 static int test_app_info(void)
@@ -74,14 +134,84 @@ static int test_pawn(void)
     return TEST_SUCCESS;
 }
 
+static int test_opponents_can_play(void)
+{
+    Board board;
+    initializeClassicGame(&board);
+    perform_move(1, 0, 3, 0, &board);
+    perform_move(6, 0, 4, 0, &board);
+    ASSERT_TEST_SUCCESS(expect_piece(&board, 3, 0, PIECE_PAWN, COLOR_BLACK));
+    ASSERT_TEST_SUCCESS(expect_piece(&board, 4, 0, PIECE_PAWN, COLOR_WHITE));
+    return TEST_SUCCESS;
+}
+
+static int test_en_passant(void)
+{
+    Board board;
+    clear_board(&board);
+    set_piece(&board, 4, 4, PIECE_PAWN, COLOR_BLACK);
+    set_piece(&board, 6, 5, PIECE_PAWN, COLOR_WHITE);
+    set_piece(&board, 0, 0, PIECE_KING, COLOR_BLACK);
+    set_piece(&board, 7, 7, PIECE_KING, COLOR_WHITE);
+
+    perform_move(6, 5, 4, 5, &board);
+    perform_move(4, 4, 5, 5, &board);
+
+    ASSERT_TEST_SUCCESS(expect_piece(&board, 5, 5, PIECE_PAWN, COLOR_BLACK));
+    ASSERT_TEST_SUCCESS(expect_piece(&board, 4, 5, PIECE_NONE, COLOR_NONE));
+    ASSERT_TEST_SUCCESS(expect_piece(&board, 4, 4, PIECE_NONE, COLOR_NONE));
+    return TEST_SUCCESS;
+}
+
+static int test_pawn_promotion_to_queen(void)
+{
+    Board board;
+    clear_board(&board);
+    set_piece(&board, 1, 0, PIECE_PAWN, COLOR_WHITE);
+    set_piece(&board, 0, 4, PIECE_KING, COLOR_BLACK);
+    set_piece(&board, 7, 7, PIECE_KING, COLOR_WHITE);
+
+    perform_move(1, 0, 0, 0, &board);
+
+    ASSERT_TEST_SUCCESS(expect_piece(&board, 0, 0, PIECE_QUEEN, COLOR_WHITE));
+    return TEST_SUCCESS;
+}
+
+static int test_check(void)
+{
+    Board board;
+    clear_board(&board);
+    set_piece(&board, 7, 4, PIECE_KING, COLOR_WHITE);
+    set_piece(&board, 0, 4, PIECE_KING, COLOR_BLACK);
+    set_piece(&board, 7, 0, PIECE_ROOK, COLOR_BLACK);
+
+    ASSERT_TRUE(king_is_in_check(&board, COLOR_WHITE));
+    return TEST_SUCCESS;
+}
+
+static int test_checkmate(void)
+{
+    Board board;
+    clear_board(&board);
+    set_piece(&board, 0, 0, PIECE_KING, COLOR_BLACK);
+    set_piece(&board, 0, 1, PIECE_PAWN, COLOR_BLACK);
+    set_piece(&board, 1, 0, PIECE_PAWN, COLOR_BLACK);
+    set_piece(&board, 1, 1, PIECE_QUEEN, COLOR_WHITE);
+    set_piece(&board, 2, 2, PIECE_BISHOP, COLOR_WHITE);
+    set_piece(&board, 7, 7, PIECE_KING, COLOR_WHITE);
+
+    ASSERT_TRUE(is_checkmate(&board, COLOR_BLACK));
+    return TEST_SUCCESS;
+}
+
 static int test_pawnCannotMoveBackwards(void) {
     
     Board board;
     initializeClassicGame(&board);
     perform_move(1, 0, 2, 0, &board);
     perform_move(2, 0, 1, 0, &board);
-    ASSERT_TEST_SUCCESS(expect_piece(&board, 1, 0, PIECE_PAWN, COLOR_BLACK));
-    ASSERT_TEST_SUCCESS(expect_piece(&board, 2, 0, PIECE_NONE, COLOR_NONE));
+    ASSERT_TEST_SUCCESS(expect_piece(&board, 1, 0, PIECE_NONE, COLOR_NONE));
+    ASSERT_TEST_SUCCESS(expect_piece(&board, 2, 0, PIECE_PAWN, COLOR_BLACK));
     return TEST_SUCCESS;
 }
 
@@ -123,6 +253,7 @@ static int test_bishop(void)
     perform_move(0, 2, 1, 1, &board);
 
     ASSERT_TEST_SUCCESS(expect_piece(&board, 1, 1, PIECE_BISHOP, COLOR_BLACK));
+    return TEST_SUCCESS;
 }
 
 static int test_queen(void)
@@ -169,12 +300,21 @@ static int test_cannotMovePinnedPiece() {
     return TEST_SUCCESS;
 }
 
+void nullFunction() {
+    
+}
+
 int main(void)
 {
     TestCase tests[] = {
         { "app info", test_app_info },
         { "classic board setup", test_classic_board_setup },
-        { "pawn", test_pawn },        
+        { "pawn", test_pawn },
+        { "opponents can play", test_opponents_can_play },
+        { "en passant", test_en_passant },
+        { "pawn promotion to queen", test_pawn_promotion_to_queen },
+        { "check", test_check },
+        { "checkmate", test_checkmate },
         { "doubleMovePawn", test_doubleMovePawn },
         { "pawnCannotMoveBackwards", test_pawnCannotMoveBackwards },
         { "rook", test_rook },
@@ -186,5 +326,5 @@ int main(void)
         { "cannotMovePinnedPiece", test_cannotMovePinnedPiece }
     };
 
-    return run_tests(tests, TEST_COUNT(tests));
+    return run_tests(tests, TEST_COUNT(tests), nullFunction, nullFunction);
 }
